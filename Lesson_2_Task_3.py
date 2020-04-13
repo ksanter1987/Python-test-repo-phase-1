cc50c22454209593f152bf322a42813ad58482a0
ksanter1987 <63009237+ksanter1987@users.noreply.github.com>
2020-04-13 14:46:16 +0300

    Lesson_2_Task_3

---
+++ Lesson_2_Task_3.py
@@ -0,0 +1,10 @@
+a = 123
+b = 31
+
+print(f'Addition: {a} + {b} = {a + b}')
+print(f'Subtraction: {a} - {b} = {a - b}')
+print(f'Division: {a} / {b} = {a / b}')
+print(f'Multiplication: {a} * {b} = {a * b}')
+print(f'Exponent (Power): {a} ** {b} = {a ** b}')
+print(f'Modulus: {a} % {b} = {a % b}')
+print(f'Floor division: {a} // {b} = {a // b}')
