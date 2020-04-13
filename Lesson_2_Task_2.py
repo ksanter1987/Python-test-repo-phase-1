9121ca12be998d52c238ca056a5f92b17c36db43
ksanter1987 <63009237+ksanter1987@users.noreply.github.com>
2020-04-13 14:29:14 +0300

    Lesson_2_Task_2

---
+++ Lesson_2_Task_2.py
@@ -0,0 +1,9 @@
+first_name = 'Oleksiy'
+last_name = 'Kuvychko'
+
+print('Good evening ' + first_name + ' ' + last_name + '. Welcome to Python lesson')
+
+print()
+
+print(f'Good evening {first_name} {last_name}. Welcome to Python lesson')
+
